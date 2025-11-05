78f4b0d45ccd46a20965bb2bb3a21fd2f4699435
Kien <trungkienvn2005@gmail.com>
2025-11-05 10:04:12 +0700

    chore:  final test

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ function App() {
     <>
       <div className='bg-red-500 font-bold text-white'>hello world</div>
       <h2>hi</h2>
-
-      <p className='flex flex-col text-red-200'>hi there</p>
     </>
   );
 }
